fe769d255dc7f8aa12d929c93214b9cc372180cd
Ege Bilecen <ege@r0m.cc>
2026-07-16 21:20:30 +0100

    added payload loading and encrypting

---
+++ packer/src/debug.rs
@@ -0,0 +1,20 @@
+#[allow(dead_code)]
+pub(crate) fn print_bytes(buf: &[u8], width: usize) {
+    for (i, b) in buf.iter().take(128).enumerate() {
+        print!("{:02X} ", b);
+
+        if (i + 1) % width == 0 {
+            println!();
+        }
+    }
+}
+
+#[allow(unused_macros)]
+macro_rules! dprintln {
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        println!($($arg)*);
+    };
+}
+#[allow(unused_imports)]
+pub(crate) use dprintln;
--- packer/src/main.rs
+++ packer/src/main.rs
@@ -1,8 +1,62 @@
-use kekkai::random_u64;
+#[cfg(debug_assertions)]
+mod debug;
+
+use argh::FromArgs;
+use indicatif::{ProgressBar, ProgressStyle};
+use std::error::Error;
+use std::path::Path;
+use std::process::ExitCode;
+use std::time::Duration;
+
 use kekkai::crypto::xor_in_place;
+use kekkai::random_u64;
 
 const XOR_KEY: u64 = random_u64!();
 
-fn main() {
+#[derive(FromArgs, Debug)]
+#[argh(description = "Kekkai (結界) - A lightweight, low-entropy binary packer.")]
+struct CliArgs {
+    #[argh(option, description = "path to the payload executable")]
+    path: String,
+}
 
+fn run(args: CliArgs) -> Result<(), Box<dyn Error>> {
+    // ─── Spinner Setup ───────────────────────────────────────────────────
+    let spinner = ProgressBar::new_spinner();
+    spinner.set_style(
+        ProgressStyle::default_spinner()
+            .tick_chars("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏")
+            .template("{spinner:.green} {msg}")?,
+    );
+    spinner.enable_steady_tick(Duration::from_millis(80));
+
+    // ─── Load The Payload ────────────────────────────────────────────────
+    spinner.set_message("Reading the payload executable...");
+    let path = Path::new(&args.path);
+
+    if !path.exists() {
+        return Err(format!("No such file found in given path: {}", args.path).into());
+    }
+
+    let mut file_data = std::fs::read(&args.path)
+        .map_err(|err| format!("Couldn't read file `{}`: {}", args.path, err))?;
+
+    // ─── Encrypt The Payload ─────────────────────────────────────────────
+    spinner.set_message("Applying low-entropy XOR obfuscation...");
+    xor_in_place(&mut file_data, &XOR_KEY.to_be_bytes());
+
+    // ─── End ─────────────────────────────────────────────────────────────
+    spinner.finish_with_message("Payload successfully obfuscated!");
+    Ok(())
+}
+
+fn main() -> ExitCode {
+    let args: CliArgs = argh::from_env();
+
+    if let Err(error) = run(args) {
+        println!("{}", error);
+        return ExitCode::FAILURE;
+    }
+
+    ExitCode::SUCCESS
 }
