a2d510c4b54e0ee8572130444ab050820f282c75
Ege Bilecen <ege@r0m.cc>
2026-07-18 19:25:53 +0100

    added initial placeholder page fault handler

--- stub/src/handlers.rs
+++ stub/src/handlers.rs
@@ -1,7 +1,21 @@
 use debug::dprintln;
-use windows_sys::Win32::System::Diagnostics::Debug::EXCEPTION_POINTERS;
+use windows_sys::Win32::{Foundation::EXCEPTION_ACCESS_VIOLATION, System::Diagnostics::Debug::{
+    EXCEPTION_CONTINUE_SEARCH, EXCEPTION_POINTERS,
+}};
 
-pub(crate) unsafe extern "system" fn page_fault_handler(_exception_info: *mut EXCEPTION_POINTERS) -> i32 {
+pub(crate) unsafe extern "system" fn page_fault_handler(
+    exception_info: *mut EXCEPTION_POINTERS,
+) -> i32 {
+    let exception_record = unsafe { exception_info.read().ExceptionRecord.read() };
+
     dprintln!("Page fault handler invoked!");
-    0
+    dprintln!("Exception code: 0x{:02X}", exception_record.ExceptionCode as usize);
+    dprintln!("Exception address: 0x{:02X}", exception_record.ExceptionAddress as usize);
+    
+    match exception_record.ExceptionCode {
+        EXCEPTION_ACCESS_VIOLATION => {
+            EXCEPTION_CONTINUE_SEARCH
+        },
+        _ => EXCEPTION_CONTINUE_SEARCH
+    }
 }
